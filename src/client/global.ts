a0b1341bc2da2f1393dccc19161c004db841b73e
nguyenphuonguyentran <trannguyenphuonguyen04072003@gmail.com>
2025-09-12 22:47:03 -0600

    chore: update global.ts

--- src/client/global.ts
+++ src/client/global.ts
@@ -1,1 +1,9 @@
 declare module '*.css';
+
+export const globalStyles = {
+  fontFamily: "'Bubblegum Sans', cursive",
+  margin: 0,
+  padding: 0,
+  boxSizing: 'border-box' as const,
+  backgroundColor: '#F9FAFB', // optional default background
+};
